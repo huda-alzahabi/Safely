277f1f187c665da642ffeb874c5fcd9197699ed0
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-30 22:58:54 +0300

    Backend: Initiated the db connection

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(DB_CONNECT, (err) => {
+    if (err) console.log(err);
+    else console.log("mongdb is connected");
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3030, () => console.log("Server running"));
