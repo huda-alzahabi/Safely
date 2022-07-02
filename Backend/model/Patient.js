9433025dbae5b69321b8cf0a9a51d532a63cf114
huda-alzahabi <hudazahabi00@gmail.com>
2022-07-02 21:42:53 +0300

    Backend: Created the patient model

---
+++ Backend/model/Patient.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+    phone_number: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    date_of_birth: {
+        type: String,
+        required: true,
+        min: 10,
+        max: 255,
+    },
+    nationality: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 255,
+    },
+    sex: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 255,
+    },
+    marital_status: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    //prescription_id
+    //address:[ {}],
+    //user_id
+});
+
+module.exports = mongoose.model("Patient", patientSchema);
