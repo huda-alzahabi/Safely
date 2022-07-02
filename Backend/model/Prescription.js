9da98ffd06e4cc0da0d796a74c64a631d4a6eec9
huda-alzahabi <hudazahabi00@gmail.com>
2022-07-02 22:21:16 +0300

    Backend: Created the prescription model

---
+++ Backend/model/Prescription.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const prescriptionSchema = new mongoose.Schema({
+    diagnosis: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    drug: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    prescription_date: {
+        type: Date,
+        required: true,
+        min: "2022-07-28",
+        max: "2022-09-23",
+    },
+    // patient_id
+    // doctor_id
+});
+
+module.exports = mongoose.model("Prescription", prescriptionSchema);
