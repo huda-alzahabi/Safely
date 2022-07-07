40782f839c81ad2b0a23a87c109d60fb37c24307
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-07 16:16:26 +0300

    Frontend: Added Welcome Activity

--- Frontend/app/src/main/java/com/finalproj/safely/WelcomeActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/WelcomeActivity.kt
@@ -1,11 +1,18 @@
 package com.finalproj.safely
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        val btn_try_now = findViewById(R.id.trynow) as Button
+        btn_try_now.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, UserTypeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
