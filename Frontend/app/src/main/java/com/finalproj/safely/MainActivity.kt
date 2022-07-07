d863b479fabdf9fa6d1b67fc8246dbf9452e8b37
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-07 12:58:19 +0300

    Created the Main Activity: Logo

--- Frontend/app/src/main/java/com/finalproj/safely/MainActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.finalproj.safely
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            delay(5000L)
+            val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+} }
+
