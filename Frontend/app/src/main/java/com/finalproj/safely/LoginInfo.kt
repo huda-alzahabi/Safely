01d518eba98a6de357352cf691960cda1c18f84c
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-08 15:53:13 +0300

    Frontend: Created LoginInfo data class

---
+++ Frontend/app/src/main/java/com/finalproj/safely/LoginInfo.kt
@@ -0,0 +1,8 @@
+package com.finalproj.safely
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginInfo(
+    @SerializedName("email") val email: String?,
+    @SerializedName("password") val password: String?
+)
