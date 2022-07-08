6a25ec7131c3e1af52148fecfe1e143bc2b905d4
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-08 15:03:26 +0300

    Frontend: Created doctors activity

---
+++ Frontend/app/src/main/java/com/finalproj/safely/DoctorsActivity.kt
@@ -0,0 +1,11 @@
+package com.finalproj.safely
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctors)
+    }
+}
