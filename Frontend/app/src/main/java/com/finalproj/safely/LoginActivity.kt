922b23b69aa2c9076cf46059d6a61d03f4ecd491
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-08 15:55:12 +0300

    Frontend: Finalized login functionality

--- Frontend/app/src/main/java/com/finalproj/safely/LoginActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/LoginActivity.kt
@@ -2,10 +2,43 @@ package com.finalproj.safely
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val login = findViewById<Button>(R.id.login_btn)
+
+        login.setOnClickListener {
+            var email = findViewById<EditText>(R.id.login_email).text.toString()
+            var password = findViewById<EditText>(R.id.login_pass).text.toString()
+
+            if (email.isEmpty()) Toast.makeText(this@LoginActivity,"Email cannot be empty!", Toast.LENGTH_SHORT).show()
+            if (password.isEmpty()) Toast.makeText(this@LoginActivity,"Password cannot be empty!",
+                Toast.LENGTH_SHORT).show()
+
+            else login(email,password)
+        }
     }
-}
+
+    private fun login(email:String, password:String) {
+
+        val apiService = RestApiService()
+        val loginInfo = LoginInfo(
+            email = email,
+            password = password)
+
+        apiService.login(loginInfo) {
+            if (loginInfo.email!= null) {
+                Log.d("OKKKKK","Successfully logged in")
+            } else {
+                Log.d("OKKKKK","Error loggin new user")
+            }
+        }
+    }
+    }
