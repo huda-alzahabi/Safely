680e63039a3764282827490eb3b8b9683dd28f58
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-08 15:03:09 +0300

    Frontend: Finalized signup functionality

--- Frontend/app/src/main/java/com/finalproj/safely/SignUpActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/SignUpActivity.kt
@@ -15,74 +15,40 @@ import org.json.JSONObject
 
 
 class SignUpActivity : AppCompatActivity() {
-  lateinit var retrofitInterface: RetrofitInterface
-    private var jsonObjectString=""
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        //initialize api
-        val retrofit= RetrofitClient.getInstance()
-        retrofitInterface=retrofit.create(RetrofitInterface::class.java)
-
-
-
         val signUp = findViewById<Button>(R.id.signup)
 
         signUp.setOnClickListener {
-            var name = findViewById<EditText>(R.id.signup_name).getText().toString()
+            var name = findViewById<EditText>(R.id.signup_name).text.toString()
             var email = findViewById<EditText>(R.id.signup_email).text.toString()
             var password = findViewById<EditText>(R.id.signup_pass).text.toString()
-            // Create JSON using JSONObject
-            val jsonObject = JSONObject()
-            jsonObject.put("name", name)
-            jsonObject.put("email", email)
-            jsonObject.put("password", password)
-            Log.d("NAMEEE",name)
-            // Convert JSONObject to String
-            jsonObjectString = jsonObject.toString()
+
             if (name.isEmpty()) Toast.makeText(this@SignUpActivity,"Name cannot be empty!",Toast.LENGTH_SHORT).show()
             if (email.isEmpty()) Toast.makeText(this@SignUpActivity,"Email cannot be empty!",Toast.LENGTH_SHORT).show()
             if (password.isEmpty()) Toast.makeText(this@SignUpActivity,"Password cannot be empty!",Toast.LENGTH_SHORT).show()
-            else registerUser()
+
+            else registerUser(name,email,password)
         }
       }
 
-    private fun registerUser() {
-        // Create RequestBody ( We're not using any converter, like GsonConverter, MoshiConverter e.t.c, that's why we use RequestBody )
-        val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+    private fun registerUser(name:String,email:String, password:String) {
 
-        CoroutineScope(Dispatchers.IO).launch {
-          try{  // Do the POST request and get response
-              delay(5000)
+            val apiService = RestApiService()
+            val userInfo = UserInfo(
+                name = name,
+                email = email,
+                password = password)
 
-            val response = retrofitInterface.registerUser(requestBody)
-
-            withContext(Dispatchers.Main) {
-
-                if (response.isSuccessful) {
-
-                    // Convert raw JSON to pretty JSON using GSON library
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-                    val prettyJson = gson.toJson(
-                        JsonParser.parseString(
-                            response.body()
-                                ?.string() // About this thread blocking annotation : https://github.com/square/retrofit/issues/3255
-                        )
-                    )
-
-                    Log.d("Pretty Printed JSON :", prettyJson)
-
+                apiService.register(userInfo) {
+                if (userInfo.email!= null) {
+                    Log.d("OKKKKK",userInfo.toString())
                 } else {
-
-                    Log.d("RETROFIT_ERROR", response.code().toString())
-
+                    Log.d("OKKKKK","Error registering new user")
                 }
             }
-        }catch(e: Exception){
-            e.printStackTrace()
-              Log.d("COROUTINEEXCEPTION", "KKKKK")
         }
-    }}}
+    }
