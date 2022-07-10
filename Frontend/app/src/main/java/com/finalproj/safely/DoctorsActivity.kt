8798cb6bc8de70ab3e3fbd9c3b925af76f8a784c
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-10 14:55:31 +0300

    Frontend: Added bottom nav to Doctors Activity

--- Frontend/app/src/main/java/com/finalproj/safely/DoctorsActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/DoctorsActivity.kt
@@ -1,11 +1,43 @@
 package com.finalproj.safely
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DoctorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctors)
+        clickedNavItem()
+    }
+    private fun clickedNavItem(){
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_home -> {
+                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_hospitals -> {
+                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_location -> {
+                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_records -> {
+                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_profile -> {
+                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                return@setOnItemSelectedListener true
+            }
     }
 }
