e64ec6e75375ac1662c220b154821ab5cab308f6
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-10 18:55:40 +0300

    Frontend: Created PatientProfile Activity with nav

---
+++ Frontend/app/src/main/java/com/finalproj/safely/PatientProfileActivity.kt
@@ -0,0 +1,47 @@
+package com.finalproj.safely
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PatientProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_profile)
+        clickedNavItem()
+    }
+
+    private fun clickedNavItem() {
+        findViewById<BottomNavigationView>(R.id.bottom_nav).menu.findItem(R.id.nav_profile).isChecked = true;
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setOnItemSelectedListener {
+                    item ->
+                when (item.itemId) {
+                    R.id.nav_home -> {
+                        val intent =
+                            Intent(this@PatientProfileActivity, PatientHomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_hospitals -> {
+                        val intent = Intent(this@PatientProfileActivity,
+                            PatientHospitalsActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_location -> {
+                        val intent = Intent(this@PatientProfileActivity, MapsActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_records -> {
+                        val intent =
+                            Intent(this@PatientProfileActivity, MedicalRecordsActivity::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.nav_profile -> {
+                       }
+                }
+                return@setOnItemSelectedListener true
+            }
+    }
+}
