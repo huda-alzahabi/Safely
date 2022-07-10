ab9d7b46b0c8fd667557962599a64ce49276c0de
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-10 18:55:24 +0300

    Frontend: Created PatientHospitals Activity with nav

---
+++ Frontend/app/src/main/java/com/finalproj/safely/PatientHospitalsActivity.kt
@@ -0,0 +1,52 @@
+package com.finalproj.safely
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PatientHospitalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_hospitals)
+
+        clickedNavItem()
+    }
+
+    private fun clickedNavItem() {
+        findViewById<BottomNavigationView>(R.id.bottom_nav).menu.findItem(R.id.nav_hospitals).isChecked = true;
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_home -> {
+                        val intent =
+                            Intent(this@PatientHospitalsActivity, PatientHomeActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(0,0);
+                    }
+                    R.id.nav_hospitals -> {
+
+                    }
+                    R.id.nav_location -> {
+                        val intent = Intent(this@PatientHospitalsActivity, MapsActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(0,0);
+                    }
+                    R.id.nav_records -> {
+                        val intent = Intent(this@PatientHospitalsActivity,
+                            MedicalRecordsActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(0,0);
+                    }
+                    R.id.nav_profile -> {
+                        val intent = Intent(this@PatientHospitalsActivity,
+                            PatientProfileActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(0,0);
+                    }
+                }
+                return@setOnItemSelectedListener true
+            }
+    }
+}
