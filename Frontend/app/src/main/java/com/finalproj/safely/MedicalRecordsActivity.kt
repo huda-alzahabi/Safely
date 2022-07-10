68bd75b995e0ce08921f5e095116c2e250ace278
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-10 18:54:28 +0300

    Frontend: Created MedicalRecords Activity with nav

--- Frontend/app/src/main/java/com/finalproj/safely/MedicalRecordsActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/MedicalRecordsActivity.kt
@@ -5,35 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class DoctorsActivity : AppCompatActivity() {
+class MedicalRecordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctors)
+        setContentView(R.layout.activity_medical_records)
         clickedNavItem()
     }
-    private fun clickedNavItem(){
+
+    private fun clickedNavItem() {
+        findViewById<BottomNavigationView>(R.id.bottom_nav).menu.findItem(R.id.nav_records).isChecked = true;
 
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setOnItemSelectedListener { item ->
                 when (item.itemId) {
                     R.id.nav_home -> {
-                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        val intent =
+                            Intent(this@MedicalRecordsActivity, PatientHomeActivity::class.java)
                         startActivity(intent)
                     }
                     R.id.nav_hospitals -> {
-                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        val intent = Intent(this@MedicalRecordsActivity,
+                            PatientHospitalsActivity::class.java)
                         startActivity(intent)
                     }
                     R.id.nav_location -> {
-                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        val intent = Intent(this@MedicalRecordsActivity, MapsActivity::class.java)
                         startActivity(intent)
                     }
                     R.id.nav_records -> {
-                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
-                        startActivity(intent)
+
                     }
                     R.id.nav_profile -> {
-                        val intent = Intent(this@DoctorsActivity, LoginActivity::class.java)
+                        val intent =
+                            Intent(this@MedicalRecordsActivity, PatientProfileActivity::class.java)
                         startActivity(intent)
                     }
                 }
