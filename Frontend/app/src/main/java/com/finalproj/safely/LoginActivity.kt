b13a353da6713a94843ce637d849faebf9cff2ec
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-12 18:58:54 +0300

    Frontend: Implemented jwt decoding

--- Frontend/app/src/main/java/com/finalproj/safely/LoginActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/LoginActivity.kt
@@ -3,13 +3,17 @@ package com.finalproj.safely
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import org.json.JSONObject
+import java.util.Base64.getUrlDecoder
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,41 +26,56 @@ class LoginActivity : AppCompatActivity() {
             var email = findViewById<EditText>(R.id.login_email).text.toString()
             var password = findViewById<EditText>(R.id.login_pass).text.toString()
 
-            if (email.isEmpty()) Toast.makeText(this@LoginActivity,"Email cannot be empty!", Toast.LENGTH_SHORT).show()
-            if (password.isEmpty()) Toast.makeText(this@LoginActivity,"Password cannot be empty!",
+            if (email.isEmpty()) Toast.makeText(this@LoginActivity,
+                "Email cannot be empty!",
                 Toast.LENGTH_SHORT).show()
-
-            else login(email,password)
+            if (password.isEmpty()) Toast.makeText(this@LoginActivity, "Password cannot be empty!",
+                Toast.LENGTH_SHORT).show()
+            else login(email, password)
         }
-        register.setOnClickListener{
+        register.setOnClickListener {
             val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 
-    private fun login(email:String, password:String) {
-        val sharedPrefFile = "kotlinsharedpreference"
+    private fun login(email: String, password: String) {
+        val sharedPrefFile = "kotlin_shared_preference"
         val apiService = RestApiService()
         val loginInfo = LoginInfo(
             email = email,
             password = password)
 
         apiService.login(loginInfo) {
-            if (it?.token!=null) {
+            if (it?.token != null) {
                 Log.d("Token", it.token)
                 val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
                     Context.MODE_PRIVATE)
-                val editor:SharedPreferences.Editor =  sharedPreferences.edit()
+
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
                 editor.putString("Token", it.token)
                 editor.apply()
                 editor.commit()
+                Log.d("DECODED:",decodeToken(it.token))
                 val intent = Intent(this@LoginActivity, PatientHomeActivity::class.java)
                 startActivity(intent)
-                finish()
             } else {
-                Log.d("Login Error","Error logging new user")
+                Log.d("Login Error", "Error logging new user")
             }
         }
     }
+
+    private fun decodeToken(token: String): String {
+        val elements = token.split('.')
+        var user_name = ""
+        if (elements.size == 3) {
+            val (header, payload, signature) = elements
+            val p = Base64.decode(payload, Base64.DEFAULT).decodeToString()
+            val json = JSONObject(p)
+             user_name = json.getString(("name").toString())
+        } else {
+            error("Invalid token")
+        }
+        return user_name
     }
+}
