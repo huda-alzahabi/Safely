91b120fc3193e18c0f719a6f986f41c8bfa5d3a9
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-16 17:10:08 +0300

    Frontend: Redirected the user to signup

--- Frontend/app/src/main/java/com/finalproj/safely/UserTypeActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/UserTypeActivity.kt
@@ -1,11 +1,30 @@
 package com.finalproj.safely
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class UserTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_type)
+
+        val patient = findViewById<Button>(R.id.patient)
+        val doctor = findViewById<Button>(R.id.doctor)
+        val hospital = findViewById<Button>(R.id.hospital)
+        patient.setOnClickListener {
+            val intent = Intent(this@UserTypeActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        doctor.setOnClickListener {
+            val intent = Intent(this@UserTypeActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        hospital.setOnClickListener {
+            val intent = Intent(this@UserTypeActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
