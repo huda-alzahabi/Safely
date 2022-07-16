5ad9cdc51e3961a41c7101a6a0a25e7675588fce
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-16 11:37:58 +0300

    Frontend: Created Book Appointment Activity

--- Frontend/app/src/main/java/com/finalproj/safely/BookAppointmentActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/BookAppointmentActivity.kt
@@ -3,12 +3,25 @@ package com.finalproj.safely
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class BookAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_appointment)
+        val datePicker = findViewById<DatePicker>(R.id.date_Picker)
+        val today = Calendar.getInstance()
+        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+
+        ) { view, year, month, day ->
+            val month = month + 1
+            val msg = "You Selected: $day/$month/$year"
+            Toast.makeText(this@BookAppointmentActivity, msg, Toast.LENGTH_SHORT).show()
+        }
         clickedNavItem()
     }
 
