170f26121298a1f46dab46f682be579d601eb213
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-16 16:58:58 +0300

    Frontend: Saved the user info from user input

--- Frontend/app/src/main/java/com/finalproj/safely/UserInfoActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/UserInfoActivity.kt
@@ -1,21 +1,73 @@
 package com.finalproj.safely
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class UserInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
-        val languages = resources.getStringArray(R.array.nationalities)
-        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown, languages)
-        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
-        autocompleteTV.setAdapter(arrayAdapter)
+        var selectedSex = ""
+        //create dropdown for nationalities
+        val nationalities = resources.getStringArray(R.array.nationalities)
+        val nationalityAdapter = ArrayAdapter(this, R.layout.dropdown, nationalities)
+        val nationality = findViewById<AutoCompleteTextView>(R.id.nationalityTextView)
+        nationality.setAdapter(nationalityAdapter)
+
+        //create dropdown for marital_status
         val marital = resources.getStringArray(R.array.marital_status)
-        val arrayAdapter2 = ArrayAdapter(this, R.layout.dropdown, marital)
-        val autocompleteTV2 = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView2)
-        autocompleteTV2.setAdapter(arrayAdapter2)
+        val maritalAdapter = ArrayAdapter(this, R.layout.dropdown, marital)
+        val maritalStatus = findViewById<AutoCompleteTextView>(R.id.maritalTextView)
+        maritalStatus.setAdapter(maritalAdapter)
+
+        val submit = findViewById<Button>(R.id.submit)
+
+        // save the selected sex
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+
+            selectedSex =  when(checkedId) {
+                R.id.radio_button_1 -> findViewById<RadioButton>(R.id.radio_button_1).text.toString()
+                R.id.radio_button_2 -> findViewById<RadioButton>(R.id.radio_button_2).text.toString()
+                R.id.radio_button_3 -> findViewById<RadioButton>(R.id.radio_button_3).text.toString()
+                else -> {"NONE" }
+            }
+        }
+        submit.setOnClickListener {
+
+            //save the phone number and the date of birth
+            var phone_num = findViewById<EditText>(R.id.phone_num).text.toString()
+            var dob = findViewById<EditText>(R.id.dob).text.toString()
+
+            // save the selected nationality
+            val selectedNationality = nationality.text.toString()
+
+            // save the selected maritalStatus
+            val selectedStatus = maritalStatus.text.toString()
+
+            submitInfo(phone_num, dob, selectedNationality, selectedStatus, selectedSex)
+
+        }
+    }
+
+    private fun submitInfo(
+        phone_num: String,
+        dob: String,
+        nationality: String,
+        maritalStatus: String,
+        sex: String,
+    ) {
+        Log.d("Phone Number", phone_num)
+        Log.d("Date of Birth", dob)
+        Log.d("Nationality", nationality)
+        Log.d("Marital Status", maritalStatus)
+        Log.d("Sex", sex)
+        val intent = Intent(this@UserInfoActivity, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
