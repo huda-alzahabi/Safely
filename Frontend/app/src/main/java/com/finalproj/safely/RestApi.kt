d1b1fd964da2727039e95fd0a20d1ba2f99797a0
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-17 20:47:41 +0300

    Frontend: Added getDrsByHospitalId

--- Frontend/app/src/main/java/com/finalproj/safely/RestApi.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/RestApi.kt
@@ -1,10 +1,10 @@
 package com.finalproj.safely
 
 
+import com.squareup.okhttp.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.Response
+import retrofit2.http.*
 
 interface RestApi {
 
@@ -15,4 +15,9 @@ interface RestApi {
     @Headers("Content-Type: application/json")
     @POST("/api/user/auth/login")
     fun login(@Body loginInfo: LoginInfo): Call<LoginResponse>
+
+    @Headers("Content-Type: application/json")
+    @GET("/api/doctor/")
+    fun getDrsByHospitalId(@Query("id") hospitalId: String?): Call<List<Doctor>>
+
 }
