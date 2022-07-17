5dfa4d2e9fbdaaed1ad2d44b8323174e7ada381a
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-17 16:25:52 +0300

    Frontend: Created Doctors Adapter

---
+++ Frontend/app/src/main/java/com/finalproj/safely/DoctorsAdapter.kt
@@ -0,0 +1,73 @@
+package com.finalproj.safely
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlin.collections.ArrayList
+
+class DoctorsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val TAG: String = "AppDebug"
+
+    private var items: List<Doctor> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BlogViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.doctors_card_view, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+
+            is BlogViewHolder -> {
+                holder.bind(items.get(position))
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(blogList: List<Doctor>) {
+        items = blogList
+    }
+
+    class BlogViewHolder
+    constructor(
+        itemView: View,
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        val txt = itemView.findViewById<View>(R.id.txt) as TextView
+        val sub_txt = itemView.findViewById<View>(R.id.sub_txt) as TextView
+        val img = itemView.findViewById<View>(R.id.img) as ImageView
+        val sub_txt2 = itemView.findViewById<View>(R.id.sub_txt2) as TextView
+
+
+        fun bind(doctor: Doctor) {
+
+            val requestOptions = RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+
+            Glide.with(itemView.context)
+                .applyDefaultRequestOptions(requestOptions)
+                .load(doctor.image)
+                .into(img)
+            txt.setText(doctor.id)
+            sub_txt.setText(doctor.name)
+            sub_txt2.setText(doctor.speciality)
+
+
+        }
+
+    }
+
+}
