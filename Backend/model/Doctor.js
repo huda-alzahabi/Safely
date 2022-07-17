accaff87da638c64ae2b51ce31ab254d04f263be
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-17 11:03:50 +0300

    Backend: Removed min and max from availability date

--- Backend/model/Doctor.js
+++ Backend/model/Doctor.js
@@ -4,8 +4,6 @@ const doctorAvailablitySchema = new mongoose.Schema({
     date: {
         type: Date,
         required: true,
-        min: "2022-07-28",
-        max: "2022-09-23",
     },
     start_time: {
         type: String,
