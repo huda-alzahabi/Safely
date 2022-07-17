13991b9769676532f818fc26d08924fc2f504f24
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-17 20:48:37 +0300

    Frontend: Returned the response of get drs

--- Frontend/app/src/main/java/com/finalproj/safely/RestApiService.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/RestApiService.kt
@@ -1,43 +1,76 @@
 package com.finalproj.safely
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.util.Log
+import android.util.Log.d
+import androidx.lifecycle.MutableLiveData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class RestApiService {
-    fun register(userData: UserInfo, onResult: (SignupResponse?) -> Unit){
+    fun register(userData: UserInfo, onResult: (SignupResponse?) -> Unit) {
         val retrofit = ServiceBuilder.buildService(RestApi::class.java)
         retrofit.register(userData).enqueue(
             object : Callback<SignupResponse> {
                 override fun onFailure(call: Call<SignupResponse>, t: Throwable) {
-                    Log.d("Added 32User",t.toString())
+                    Log.d("Added 32User", t.toString())
                     onResult(null)
                 }
-                override fun onResponse( call: Call<SignupResponse>, response: Response<SignupResponse>) {
+
+                override fun onResponse(
+                    call: Call<SignupResponse>,
+                    response: Response<SignupResponse>,
+                ) {
                     val addedUser = response.body()
-                    Log.d("Added User",response.body().toString())
+                    Log.d("Added User", response.body().toString())
                     onResult(addedUser)
 
 
                 }
-           })    }
-    fun login(loginInfo: LoginInfo, onResult: (LoginResponse?) -> Unit){
+            })
+    }
+
+    fun login(loginInfo: LoginInfo, onResult: (LoginResponse?) -> Unit) {
         val retrofit = ServiceBuilder.buildService(RestApi::class.java)
         retrofit.login(loginInfo).enqueue(
             object : Callback<LoginResponse> {
                 override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
                     onResult(null)
                 }
-                override fun onResponse( call: Call<LoginResponse>, response: Response<LoginResponse>) {
+
+                override fun onResponse(
+                    call: Call<LoginResponse>,
+                    response: Response<LoginResponse>,
+                ) {
                     val addedUser = response.body()
                     onResult(addedUser)
                     if (response.code() == 400) {
                         response.errorBody()?.let { Log.e("error", it.string()) }
                     }
-                    Log.d("User",response.toString())
+                    Log.d("User", response.toString())
                 }
             }
         )
+    }
+
+    fun getDrs(onResult: (List<Doctor?>) -> Unit) {
+        val retrofitInstance = ServiceBuilder.buildService(RestApi::class.java)
+        retrofitInstance.getDrsByHospitalId("62c9894d6393fe22e5cfa344")
+            .enqueue(object : Callback<List<Doctor>?> {
+                override fun onResponse(
+                    call: Call<List<Doctor>?>,
+                    response: Response<List<Doctor>?>,
+                ) {
+                    val responseBody = response.body()!!
+                    onResult(responseBody)
+                    Log.d("DOCTORSSS", responseBody.toString())
+                }
+
+                override fun onFailure(call: Call<List<Doctor>?>, t: Throwable) {
+                    d(TAG, "onFailure: " + t.message) //แสดง log onFailure
+                }
+            })
     }
 }
