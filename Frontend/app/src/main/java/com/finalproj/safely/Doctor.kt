a0e44fdb529243686860d80baaec514d1427b841
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-18 17:04:57 +0300

    Frontend: Created availability dataclass for Doctor

--- Frontend/app/src/main/java/com/finalproj/safely/Doctor.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/Doctor.kt
@@ -5,11 +5,31 @@ data class Doctor(
     var name: String,
     var image: String,
     var specialty: String,
+    var availability: ArrayList<Availability>
 
     ) {
 
     override fun toString(): String {
         return "Doctor(id='$_id',  name='$name',image='$image', specialty='$specialty')"
+    }
+    data class Availability(
+        var _id: String,
+        var doctor_id: String,
+        var date: String,
+        var time: String,
+        var user_appointment: AppointmentStatus
+    ) {
+        override fun toString(): String {
+            return "Availability(id='$_id', doctor_id='$doctor_id', time='$time', date='$date')"
+        }
+        data class AppointmentStatus(
+            var user: String,
+            var available: Boolean
+        ) {
+            override fun toString(): String {
+                return "Appointment(user_id='$user', available='$available')"
+            }
+        }
     }
 
 
