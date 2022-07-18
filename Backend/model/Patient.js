2c8f9efeb4fb5160b4cccc3bbaf002fbd22821ab
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-19 00:24:23 +0300

    Backend: Added medical records to the patient schema

--- Backend/model/Patient.js
+++ Backend/model/Patient.js
@@ -1,51 +1,76 @@
 const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema({
-    phone_number: {
+  phone_number: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  date_of_birth: {
+    type: String,
+    required: true,
+    min: 10,
+    max: 255,
+  },
+  nationality: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 255,
+  },
+  sex: {
+    type: String,
+    required: true,
+    min: 4,
+    max: 255,
+  },
+  marital_status: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  locations: [
+    {
+      longitude: {
         type: String,
         required: true,
-        min: 6,
-        max: 255,
-    },
-    date_of_birth: {
+      },
+      latitude: {
         type: String,
         required: true,
-        min: 10,
-        max: 255,
+      },
     },
-    nationality: {
+  ],
+  medical_records: [
+    {
+      current_medications: {
         type: String,
         required: true,
-        min: 5,
-        max: 255,
-    },
-    sex: {
+      },
+      chronic_diseases: {
         type: String,
         required: true,
-        min: 4,
-        max: 255,
-    },
-    marital_status: {
+      },
+      allergies: {
         type: String,
         required: true,
-        min: 6,
-        max: 255,
+      },
+      previous_surgeries: {
+        type: String,
+        required: true,
+      },
+      family_history: {
+        type: String,
+        required: true,
+      },
     },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    locations: [{
-        longitude:{
-            type: String,
-            required: true,
-        }
-        ,
-        latitude:{
-            type: String,
-            required: true,
-        }
-    }]
+  ],
 });
 
 module.exports = mongoose.model("Patient", patientSchema);
