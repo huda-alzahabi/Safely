ae59e73eb47f83379d90e3852043b42516902bd0
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-18 21:30:55 +0300

    Backend: Created edit profile api

--- Backend/src/user/controller.js
+++ Backend/src/user/controller.js
@@ -1,69 +1,92 @@
 const { getUsers, getById, addUser, getByEmail } = require("./service");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../../model/User");
+
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 
 async function get(req, res) {
-    try {
-        console.log(req.query);
+  try {
+    console.log(req.query);
 
-        if (req.query.id) {
-            const id = req.query.id;
-            const result = await getById(id);
-            console.log("result of specific user =>", result);
-            return res.send(result);
-        }
-
-        const result = await getUsers();
-        console.log("result =>", result);
-
-        return res.send(result);
-    } catch (error) {
-        console.log(error);
+    if (req.query.id) {
+      const id = req.query.id;
+      const result = await getById(id);
+      console.log("result of specific user =>", result);
+      return res.send(result);
     }
+
+    const result = await getUsers();
+    console.log("result =>", result);
+
+    return res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function register(req, res) {
-    try {
-        console.log(req.body);
+  try {
+    console.log(req.body);
 
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-        const addUserResult = await addUser(req.body, hashPassword);
-        console.log("addUserResult =>", addUserResult);
+    const addUserResult = await addUser(req.body, hashPassword);
+    console.log("addUserResult =>", addUserResult);
 
-        return res.send({ user: addUserResult._id });
-    } catch (error) {
-        console.log(error);
-    }
+    return res.send({ user: addUserResult._id });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function login(req, res) {
-    try {
-        const user = await getByEmail(req.body.email);
-        if (!user) return res.status(400).send("invalid credentials");
+  try {
+    const user = await getByEmail(req.body.email);
+    if (!user) return res.status(400).send("invalid credentials");
 
-        const validPassword = await bcrypt.compare(
-            req.body.password,
-            user.password
-        );
-        if (!validPassword) return res.status(400).send("invalid credentials");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) return res.status(400).send("invalid credentials");
 
-        const token = jwt.sign({ _id: user._id, name: user.name, email: user.email },
-            TOKEN_SECRET
-        );
-        // user.token = token;
+    const token = jwt.sign(
+      { _id: user._id, name: user.name, email: user.email },
+      TOKEN_SECRET
+    );
 
-        return res.status(200).json({ token: token });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
+    return res.status(200).json({ token: token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+async function editProfile(req, res) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = await User.findByIdAndUpdate(
+      { _id: req.query.id },
+      {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          password: hashPassword,
+        },
+      }
+    );
+    return res.send("Contact Successfully Updated");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    get,
-    register,
-    login,
+  get,
+  register,
+  login,
+  editProfile,
 };
--- Backend/src/user/index.js
+++ Backend/src/user/index.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const {get, register, login } = require("./controller");
+const {get, register, login,editProfile} = require("./controller");
 const router = Router();
 router.get("/auth/get", get);
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+router.post("/profile", editProfile);
 
 module.exports = router;
