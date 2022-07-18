a4fb412ea3c35f8c90dd8b81c6b4fd93dba3479f
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-18 13:09:54 +0300

    Backend: Implemented addAddress function

--- Backend/src/hospital/service.js
+++ Backend/src/hospital/service.js
@@ -1,16 +1,30 @@
 const Hospital = require("../../model/Hospital");
+const Address = require("../../model/Address");
 
 async function addHospital(body) {
-    const { phone_number, user } = body;
+  const { phone_number, user } = body;
 
-    const hospital = new Hospital({
-        phone_number,
-        user,
-    });
+  const hospital = new Hospital({
+    phone_number,
+    user,
+  });
 
-    return await hospital.save();
+  return await hospital.save();
 }
+async function addAddress(body) {
+  const { country, city, street, building, hospital } = body;
 
+  const address = new Address({
+    country,
+    city,
+    street,
+    building,
+    hospital,
+  });
+  console.log("address =>", address);
+  return await address;
+}
 module.exports = {
-    addHospital,
+  addHospital,
+  addAddress,
 };
