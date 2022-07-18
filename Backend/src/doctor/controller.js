502de1972e19b0ba01b545ba5e887b440b20e39b
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-18 13:09:20 +0300

    Backend: Formatted code

--- Backend/src/doctor/controller.js
+++ Backend/src/doctor/controller.js
@@ -1,73 +1,75 @@
-const { addDoctor,getDoctorsByHospitalId ,addAvailability} = require("./service");
+const {
+  addDoctor,
+  getDoctorsByHospitalId,
+  addAvailability,
+} = require("./service");
 const Hospital = require("../../model/Hospital");
 const Doctor = require("../../model/Doctor");
 
-
-
 async function addDr(req, res) {
-    try {
-        const newDoctor = await addDoctor(req.body);
-        console.log("newDoctor =>", newDoctor);
+  try {
+    const newDoctor = await addDoctor(req.body);
+    console.log("newDoctor =>", newDoctor);
 
-        const updateHospital = await Hospital.updateOne(
-            {
-              _id: newDoctor.hospital,
-            },
-            {
-              $push: {
-                doctors: newDoctor._id,
-              },
-            }
-          );
-          console.log("updateHospital =>", updateHospital);
-      
-        return res.status(200).send(newDoctor); 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
+    const updateHospital = await Hospital.updateOne(
+      {
+        _id: newDoctor.hospital,
+      },
+      {
+        $push: {
+          doctors: newDoctor._id,
+        },
+      }
+    );
+    console.log("updateHospital =>", updateHospital);
+
+    return res.status(200).send(newDoctor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
 
 async function getDoctors(req, res) {
-    try {
-      console.log(req.query);
-  
-      if (req.query.id) {
-        const id = req.query.id;
-        const result = await getDoctorsByHospitalId(id);
-        console.log("result of specific hospital =>", result);
-        return res.send(result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  try {
+    console.log(req.query);
 
-  async function addDrAvailability(req, res){ 
-    try {
-      const newAvailability = await addAvailability(req.body);
-      console.log("newAvailability =>", newAvailability);
-      const updateDr = await Doctor.updateOne(
-        {
-          _id: newAvailability.doctor,
-        },
-        {
-          $push: {
-            availability: newAvailability,
-          },
-        }
-      );
-      console.log("updateDr =>", updateDr);
-  
-      return res.status(200).send(newAvailability);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
+    if (req.query.id) {
+      const id = req.query.id;
+      const result = await getDoctorsByHospitalId(id);
+      console.log("result of specific hospital =>", result);
+      return res.send(result);
     }
+  } catch (error) {
+    console.log(error);
   }
-  
+}
+
+async function addDrAvailability(req, res) {
+  try {
+    const newAvailability = await addAvailability(req.body);
+    console.log("newAvailability =>", newAvailability);
+    const updateDr = await Doctor.updateOne(
+      {
+        _id: newAvailability.doctor,
+      },
+      {
+        $push: {
+          availability: newAvailability,
+        },
+      }
+    );
+    console.log("updateDr =>", updateDr);
+
+    return res.status(200).send(newAvailability);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
 module.exports = {
-    addDr,
-    addDrAvailability,
-  getDoctors
+  addDr,
+  addDrAvailability,
+  getDoctors,
 };
--- Backend/src/doctor/service.js
+++ Backend/src/doctor/service.js
@@ -1,49 +1,34 @@
 const Doctor = require("../../model/Doctor");
-const DrAvailability= require("../../model/DoctorAvailability");
-
+const DrAvailability = require("../../model/DoctorAvailability");
 
 async function addDoctor(body) {
-  const {
+  const { name, specialty, hospital } = body;
+
+  const doctor = new Doctor({
     name,
     specialty,
     hospital,
-   
-} = body;
+  });
 
-const doctor = new Doctor({
-    name,
-    specialty,
-    hospital,
-}); 
-
-    return await doctor.save();
+  return await doctor.save();
 }
-  async function getDoctorsByHospitalId(hospital_id) {
-    return await Doctor.find({ hospital: hospital_id });
-  }
-  async function addAvailability(body) {
-    const {
-      date,
-      start_time,
-      end_time,
-      doctor,
-     
-  } = body;
+async function getDoctorsByHospitalId(hospital_id) {
+  return await Doctor.find({ hospital: hospital_id });
+}
+async function addAvailability(body) {
+  const { date, time, doctor } = body;
 
   const availability = new DrAvailability({
-      date,
-      start_time,
-      end_time,
-      doctor,
-  }); 
+    date,
+    time,
+    doctor,
+  });
   console.log("availability =>", availability);
   return await availability;
-  }
-
-
+}
 
 module.exports = {
-    addDoctor,
-    addAvailability,
-    getDoctorsByHospitalId
+  addDoctor,
+  addAvailability,
+  getDoctorsByHospitalId,
 };
