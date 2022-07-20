8239041d44d0b9c3401dfd99b8f71c3236b1ea91
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-20 23:39:09 +0300

    Backend: Modified hospital address schema

--- Backend/model/Address.js
+++ Backend/model/Address.js
@@ -3,25 +3,25 @@ const mongoose = require("mongoose");
 const addressSchema = new mongoose.Schema({
     country: {
         type: String,
-        required: true,
+        required: false,
         min: 6,
         max: 255,
     },
     city: {
         type: String,
-        required: true,
+        required: false,
         min: 6,
         max: 255,
     },
     street: {
         type: String,
-        required: true,
+        required: false,
         min: 6,
         max: 255,
     },
     building: {
         type: String,
-        required: true,
+        required: false,
         min: 6,
         max: 255,
     },
