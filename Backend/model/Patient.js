ba3aa6e18e707a0d94d9c2205a6f1a126bfc7a69
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-20 10:38:21 +0300

    Backend: Added additional notes for patient

--- Backend/model/Patient.js
+++ Backend/model/Patient.js
@@ -69,6 +69,10 @@ const patientSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+      additional_notes: {
+        type: String,
+        required: true,
+      },
     },
   
 });
