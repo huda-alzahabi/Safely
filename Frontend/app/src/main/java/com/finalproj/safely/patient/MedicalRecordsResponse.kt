945fe6dd4102c63ffbbd1bc38898d16144fdde40
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-20 19:11:35 +0300

    Frontend: Created MedicalRecords response

---
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/MedicalRecordsResponse.kt
@@ -0,0 +1,6 @@
+package com.finalproj.safely.patient
+
+import com.google.gson.annotations.SerializedName
+
+data class MedicalRecordsResponse(
+    @SerializedName("message") val message: String?,)
