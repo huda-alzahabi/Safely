1787a91e7357718b5df362c974dce71a6ded843e
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-20 20:52:52 +0300

    Frontend: SuccessMessageResponse data class

--- Frontend/app/src/main/java/com/finalproj/safely/patient/SuccessMessageResponse.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/SuccessMessageResponse.kt
@@ -2,5 +2,5 @@ package com.finalproj.safely.patient
 
 import com.google.gson.annotations.SerializedName
 
-data class MedicalRecordsResponse(
+data class SuccessMessageResponse(
     @SerializedName("message") val message: String?,)
