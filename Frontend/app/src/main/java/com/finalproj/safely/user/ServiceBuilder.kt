9b4826523d92cc204ad9db0004bb1fc2819f9824
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-21 11:21:04 +0300

    Frontend: Added gson builder

--- Frontend/app/src/main/java/com/finalproj/safely/user/ServiceBuilder.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/user/ServiceBuilder.kt
@@ -1,15 +1,21 @@
 package com.finalproj.safely.user
 
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
 
+    var gson = GsonBuilder()
+        .setLenient()
+        .create()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("http://192.168.101.121:3030")
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .client(client)
         .build()
 
