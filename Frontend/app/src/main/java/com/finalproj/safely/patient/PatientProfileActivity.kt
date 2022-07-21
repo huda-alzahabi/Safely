675a194238e686020f78db23fc8937fbf120a73c
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-21 11:32:02 +0300

    Frontend: Added Logout functionality

--- Frontend/app/src/main/java/com/finalproj/safely/patient/PatientProfileActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/PatientProfileActivity.kt
@@ -1,18 +1,48 @@
 package com.finalproj.safely.patient
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.finalproj.safely.R
+import com.finalproj.safely.user.MainActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class PatientProfileActivity : AppCompatActivity() {
+    private val PREFERENCES = "kotlin_shared_preference"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_profile)
         val submit_btn=findViewById<Button>(R.id.submit_changes)
+
+        submit_btn.setOnClickListener{
+            editProfile()
+            val intent = Intent(this, PatientHomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val logout= findViewById<ImageView>(R.id.logout_icon)
+        logout.setOnClickListener{
+            logout()
+            Log.d("logout", "logout")
+        }
         clickedNavItem()
+    }
+
+    private fun editProfile() {
+        TODO("Not yet implemented")
+    }
+
+    private fun logout(){
+        this.getSharedPreferences(PREFERENCES, 0).edit().clear().apply();
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun clickedNavItem() {
@@ -38,7 +68,7 @@ class PatientProfileActivity : AppCompatActivity() {
                     }
                     R.id.nav_records -> {
                         val intent =
-                            Intent(this@PatientProfileActivity, MedicalRecordsActivity::class.java)
+                            Intent(this@PatientProfileActivity, AddMedicalRecordsActivity::class.java)
                         startActivity(intent)
                     }
                     R.id.nav_profile -> {
---
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/ViewMedicalRecordsActivity.kt
@@ -0,0 +1,12 @@
+package com.finalproj.safely.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finalproj.safely.R
+
+class ViewMedicalRecordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_medical_records)
+    }
+}
