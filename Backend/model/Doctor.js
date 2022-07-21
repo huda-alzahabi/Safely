095745d560a94c87e973badf8c33b4fd4f2570ce
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-21 23:40:17 +0300

    Backend: Modified the doctor schema

--- Backend/model/Doctor.js
+++ Backend/model/Doctor.js
@@ -1,24 +1,21 @@
 const mongoose = require("mongoose");
-const drAvailability=require("../model/DoctorAvailability");
+const drAvailability = require("../model/DoctorAvailability");
 const doctorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255,
-    },
-    specialty: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255,
-    },
-    hospital: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Hospital",
-    },
  
-    availability:[drAvailability.schema]
+  specialty: {
+    type: String,
+    required: true,
+  },
+  years_of_experience: {
+    type: String,
+    required: true,
+  },
+  hospital: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hospital",
+  },
+
+  availability: [drAvailability.schema],
 });
 
 module.exports = mongoose.model("Doctor", doctorSchema);
