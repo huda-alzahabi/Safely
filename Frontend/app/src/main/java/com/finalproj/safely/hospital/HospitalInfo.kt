ee07bf65892c8cff59e1cde9dc4698401720c596
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-21 19:05:34 +0300

    Frontend: Created HospitalInfo data class

---
+++ Frontend/app/src/main/java/com/finalproj/safely/hospital/HospitalInfo.kt
@@ -0,0 +1,12 @@
+package com.finalproj.safely.hospital
+
+import com.google.gson.annotations.SerializedName
+
+data class HospitalInfo(
+    @SerializedName("phone_number") val phone_number: String?,
+    @SerializedName("country") val country: String?,
+    @SerializedName("city") val city: String?,
+    @SerializedName("street") val street: String?,
+    @SerializedName("building") val building: String?,
+    @SerializedName("user") val user: String?,
+)
