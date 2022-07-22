b7554950acfe07d9613302641e2c54344f325330
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-22 14:24:10 +0300

    Frontend: Renamed location data class

--- Frontend/app/src/main/java/com/finalproj/safely/patient/Location.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/Location.kt
@@ -2,7 +2,7 @@ package com.finalproj.safely.patient
 
 import com.google.gson.annotations.SerializedName
 
-data class PatientLocation(
+data class Location(
     @SerializedName("longitude") val longitude: String?,
     @SerializedName("latitude") val latitude: String?
 )
