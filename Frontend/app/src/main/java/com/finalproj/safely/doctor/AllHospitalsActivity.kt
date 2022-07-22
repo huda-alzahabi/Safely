6e929cc5c59aa5a0fffa0864cf838e97e4c06df3
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-23 00:28:24 +0300

    Frontend: Implemented onItemClick for hospitals

--- Frontend/app/src/main/java/com/finalproj/safely/doctor/AllHospitalsActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/doctor/AllHospitalsActivity.kt
@@ -1,29 +1,36 @@
 package com.finalproj.safely.doctor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.finalproj.safely.R
+import com.finalproj.safely.patient.Doctor
 import com.finalproj.safely.patient.Hospital
+import com.finalproj.safely.patient.PatientDoctorsActivity
 import com.finalproj.safely.patient.TopSpacingItemDecoration
+import com.finalproj.safely.user.BookAppointmentActivity
 import com.finalproj.safely.user.RestApiService
 
-class AllHospitalsActivity : AppCompatActivity() {
+class AllHospitalsActivity : AppCompatActivity(), HospitalsAdapter.OnItemClickListener {
     private lateinit var hospitalsAdapter: HospitalsAdapter
+    private lateinit var hospitalsList: List<Hospital>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_hospitals)
         val apiService = RestApiService()
         apiService.getHospitals() {
+            hospitalsList = it as List<Hospital>
             Log.d("hospitals", it.toString())
-            hospitalsAdapter.submitList(it as List<Hospital>)
+            hospitalsAdapter.submitList(hospitalsList)
 
         }
         initRecyclerView()
     }
+
     private fun initRecyclerView() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.hospital_recycler_view)
@@ -31,8 +38,15 @@ class AllHospitalsActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@AllHospitalsActivity)
             val topSpacingDecorator = TopSpacingItemDecoration(30)
             addItemDecoration(topSpacingDecorator)
-            hospitalsAdapter = HospitalsAdapter()
+            hospitalsAdapter = HospitalsAdapter(this@AllHospitalsActivity)
             adapter = hospitalsAdapter
         }
+    }
+
+    override fun onItemClick(position: Int) {
+        val intent = Intent(this, PatientDoctorsActivity::class.java)
+        val clickedItem: Hospital = hospitalsList[position]
+        hospitalsAdapter.notifyItemChanged(position)
+        startActivity(intent)
     }
 }
