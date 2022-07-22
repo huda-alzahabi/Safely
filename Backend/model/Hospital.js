fffdc12e2bc685165e88e302d1e86ff3804a80d1
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-22 14:26:21 +0300

    Backend: Modified the hospital schema

--- Backend/model/Hospital.js
+++ Backend/model/Hospital.js
@@ -1,37 +1,36 @@
 const mongoose = require("mongoose");
-const addressSchema=require("../model/Address").schema;
+const addressSchema = require("../model/Address").schema;
 
 const hospitalSchema = new mongoose.Schema({
-    phone_number: {
-        type: String,
-        required: false,
-        min: 6,
-        max: 255,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    address: [addressSchema],
+  phone_number: {
+    type: String,
+    required: false,
+    min: 6,
+    max: 255,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  address: addressSchema,
 
-    location: {
-        longitude:{
-            type: String,
-            required: false,
-        }
-        ,
-        latitude:{
-            type: String,
-            required: false,
-        }
+  hospital_location: {
+    longitude: {
+      type: String,
+      required: false,
     },
-    doctors:
-        [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Doctor",
-        }],
-    
+    latitude: {
+      type: String,
+      required: false,
+    },
+  },
+  doctors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+    },
+  ],
 });
+hospitalSchema.index({'hospital_location':"2dsphere"});
 
-hospitalSchema.index({location:"2dsphere"});
 module.exports = mongoose.model("Hospital", hospitalSchema);
