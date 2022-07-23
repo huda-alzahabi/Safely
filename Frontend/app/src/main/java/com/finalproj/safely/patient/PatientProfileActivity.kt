55f6b674d25462d7158050b921710248a4b56d49
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-23 23:45:09 +0300

    Frontend: Implemented editProfile in patient

--- Frontend/app/src/main/java/com/finalproj/safely/patient/PatientProfileActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/PatientProfileActivity.kt
@@ -11,84 +11,103 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.finalproj.safely.R
 import com.finalproj.safely.user.MainActivity
+import com.finalproj.safely.user.RestApiService
+import com.finalproj.safely.user.UserInfo
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class PatientProfileActivity : AppCompatActivity() {
     private val sharedPrefFile = "kotlin_shared_preference"
     lateinit var sharedPreferences: SharedPreferences
+    lateinit var user: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_profile)
 
-        val submit_btn=findViewById<Button>(R.id.submit_changes)
-        val edit_name=findViewById<EditText>(R.id.edit_name)
-        val edit_email=findViewById<EditText>(R.id.edit_email)
+        val submit_btn = findViewById<Button>(R.id.submit_changes)
+        val edit_name = findViewById<EditText>(R.id.edit_name)
+        val edit_email = findViewById<EditText>(R.id.edit_email)
 
         sharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
         val name = sharedPreferences.getString("Name", "")!!
         val email = sharedPreferences.getString("Email", "")!!
+        val usertype = sharedPreferences.getString("user_type", "")!!
+        user = sharedPreferences.getString("user_id", "")!!
 
         edit_name.setText(name)
         edit_email.setText(email)
+        submit_btn.setOnClickListener {
+            val edit_name = findViewById<EditText>(R.id.edit_name).text.toString()
+            val edit_email = findViewById<EditText>(R.id.edit_email).text.toString()
+            val edit_pass = findViewById<EditText>(R.id.edit_pass).text.toString()
 
-
-        submit_btn.setOnClickListener{
-            editProfile()
+            editProfile(edit_name, edit_email, edit_pass, usertype)
             val intent = Intent(this, PatientHomeActivity::class.java)
             startActivity(intent)
         }
 
-        val logout= findViewById<ImageView>(R.id.logout_icon)
-        logout.setOnClickListener{
+        val logout = findViewById<ImageView>(R.id.logout_icon)
+        logout.setOnClickListener {
             logout()
             Log.d("logout", "logout")
         }
         clickedNavItem()
     }
 
-    private fun editProfile() {
-        TODO("Not yet implemented")
+    private fun editProfile(
+        editedName: String,
+        editedEmail: String,
+        editedPass: String,
+        usertype: String,
+    ) {
+        val apiService = RestApiService()
+        val userInfo = UserInfo(editedName, editedEmail, editedPass, usertype)
+        apiService.editUserProfile(user, userInfo) {
+            if (it != null) {
+                Log.d("User Updated", it.toString())
+            } else {
+                Log.d("NOO", "Error adding new patient")
+            }
+        }
     }
 
-    private fun logout(){
+    private fun logout() {
         this.getSharedPreferences(sharedPrefFile, 0).edit().clear().apply();
-        val intent= Intent(this,MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
     private fun clickedNavItem() {
-        val bottomnav=findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val bottomnav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomnav.menu.findItem(R.id.nav_profile).isChecked = true;
 
-        bottomnav.setOnItemSelectedListener {
-                    item ->
-                when (item.itemId) {
-                    R.id.nav_home -> {
-                        val intent =
-                            Intent(this@PatientProfileActivity, PatientHomeActivity::class.java)
-                        startActivity(intent)
-                    }
-                    R.id.nav_hospitals -> {
-                        val intent = Intent(this@PatientProfileActivity,
-                            NearbyHospitalsActivity::class.java)
-                        startActivity(intent)
-                    }
-                    R.id.nav_location -> {
-                        val intent = Intent(this@PatientProfileActivity, MapsActivity::class.java)
-                        startActivity(intent)
-                    }
-                    R.id.nav_records -> {
-                        val intent =
-                            Intent(this@PatientProfileActivity, AddMedicalRecordsActivity::class.java)
-                        startActivity(intent)
-                    }
-                    R.id.nav_profile -> {
-                       }
+        bottomnav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    val intent =
+                        Intent(this@PatientProfileActivity, PatientHomeActivity::class.java)
+                    startActivity(intent)
                 }
-                return@setOnItemSelectedListener true
+                R.id.nav_hospitals -> {
+                    val intent = Intent(this@PatientProfileActivity,
+                        NearbyHospitalsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_location -> {
+                    val intent = Intent(this@PatientProfileActivity, MapsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_records -> {
+                    val intent =
+                        Intent(this@PatientProfileActivity, AddMedicalRecordsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_profile -> {
+                }
             }
+            return@setOnItemSelectedListener true
+        }
     }
 }
