5820821f6606d57deb4c75d9752fae4d135fdb94
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-23 10:04:49 +0300

    Frontend: Added alertdialog for unsuccessful signup

--- Frontend/app/src/main/java/com/finalproj/safely/user/SignUpActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/user/SignUpActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.finalproj.safely.R
 import com.finalproj.safely.doctor.AllHospitalsActivity
@@ -17,12 +18,19 @@ import com.finalproj.safely.patient.PatientInfoActivity
 
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var builder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         val signUp = findViewById<Button>(R.id.signup)
+        builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.signup_failed)
+        builder.setMessage(R.string.email_in_use)
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("Exit") { dialogInterface, which ->
+        }
 
         signUp.setOnClickListener {
             var name = findViewById<EditText>(R.id.signup_name).text.toString()
@@ -46,40 +54,44 @@ class SignUpActivity : AppCompatActivity() {
         val sharedPrefFile = "kotlin_shared_preference"
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
+        val usertype = sharedPreferences.getString("usertype", "")
 
         val apiService = RestApiService()
         val userInfo = UserInfo(
             name = name,
             email = email,
             password = password,
+            userType = usertype
         )
 
         apiService.register(userInfo) {
             Log.d("User", userInfo.toString())
-            if (it.toString() != null) {
+            if (it != null) {
                 Log.d("HEYYY", it.toString())
                 val editor: SharedPreferences.Editor = sharedPreferences.edit()
                 editor.putString("Name", name)
                 editor.apply()
                 editor.commit()
+                if (usertype != null) {
+                    Log.d("Usertype", usertype)
+                }
+                if (usertype == "patient") {
+                    val intent = Intent(this, PatientInfoActivity::class.java)
+                    startActivity(intent)
+                } else if (usertype == "doctor") {
+                    val intent = Intent(this, AllHospitalsActivity::class.java)
+                    startActivity(intent)
+                } else if (usertype == "hospital") {
+                    val intent = Intent(this, HospitalHomeActivity::class.java)
+                    startActivity(intent)
+                }
             } else {
                 Log.d("OKKKKK", "Error registering new user")
-            }
-        }
 
-        val usertype = sharedPreferences.getString("usertype", "")
-        if (usertype != null) {
-            Log.d("Usertype", usertype)
-        }
-        if (usertype == "patient") {
-            val intent = Intent(this, PatientInfoActivity::class.java)
-            startActivity(intent)
-        } else if (usertype == "doctor") {
-            val intent = Intent(this, AllHospitalsActivity::class.java)
-            startActivity(intent)
-        }else if (usertype == "hospital") {
-            val intent = Intent(this, HospitalHomeActivity::class.java)
-            startActivity(intent)
+                val alertDialog: AlertDialog = builder.create()
+                alertDialog.setCancelable(false)
+                alertDialog.show()
+            }
         }
     }
 }
