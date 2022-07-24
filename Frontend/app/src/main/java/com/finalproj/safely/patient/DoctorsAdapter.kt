a1e6fc224fcd4f6acad4da1459043f2174b30b64
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-24 08:19:29 +0300

    Frontend: Implemented getFilter in DoctorsAdapter

--- Frontend/app/src/main/java/com/finalproj/safely/patient/DoctorsAdapter.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/DoctorsAdapter.kt
@@ -3,6 +3,8 @@ package com.finalproj.safely.patient
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -10,10 +12,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.finalproj.safely.R
+import java.util.*
 import kotlin.collections.ArrayList
 
-class DoctorsAdapter(private val listener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DoctorsAdapter(private val listener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
+    Filterable {
     private var items: List<Doctor> = ArrayList()
+    private var fullList: List<Doctor> = ArrayList()
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return DoctorViewHolder(
@@ -36,6 +41,7 @@ class DoctorsAdapter(private val listener: OnItemClickListener) : RecyclerView.A
 
     fun submitList(doctorList: List<Doctor>) {
         items = doctorList
+        fullList = doctorList
         return notifyDataSetChanged()
     }
 
@@ -74,6 +80,31 @@ class DoctorsAdapter(private val listener: OnItemClickListener) : RecyclerView.A
             txt.setText(doctor.user.name)
             sub_txt.setText(doctor.specialty)
 
+        }
+    }
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList: MutableList<Doctor> = ArrayList()
+
+                if (constraint == null || constraint.isEmpty()) {
+                    filteredList.addAll(fullList)
+                } else {
+                    val filterPattern = constraint.toString().lowercase(Locale.ROOT).trim { it <= ' ' }
+                    for (item in fullList) {
+                        if (item.user.name?.lowercase(Locale.ROOT)!!.contains(filterPattern)) {
+                            filteredList.add(item)
+                        }
+                    }
+                }
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                items = results?.values as List<Doctor>
+                notifyDataSetChanged()
+            }
         }
     }
 
