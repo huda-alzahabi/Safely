42acde67de8beaf6098143f5eeecac6698830fd3
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-25 23:33:51 +0300

    Frontend: Created AppointmentInfo data class

---
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/AppointmentInfo.kt
@@ -0,0 +1,14 @@
+package com.finalproj.safely.patient
+
+import com.google.gson.annotations.SerializedName
+
+data class AppointmentInfo(
+    @SerializedName("date") val date: String,
+    @SerializedName("time") val time: String,
+    @SerializedName("hospital_name") var hospital_name: String,
+    @SerializedName("hospital_id") var hospital_id: String,
+    @SerializedName("patient_name") var patient_name: String,
+    @SerializedName("patient_id") var patient_id: String,
+    @SerializedName("doctor_name") var doctor_name: String,
+    @SerializedName("doctor_id") var doctor_id: String,
+)
