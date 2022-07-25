31bb82492f0f785f5ba54f98cc20e1cd72e190a9
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-25 14:22:43 +0300

    Frontend: Redirected the user after MainActivity

--- Frontend/app/src/main/java/com/finalproj/safely/user/MainActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/user/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.finalproj.safely.user
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import kotlinx.coroutines.GlobalScope
 import androidx.appcompat.app.AppCompatActivity
 import com.finalproj.safely.R
+import com.finalproj.safely.doctor.AddAvailabilityActivity
+import com.finalproj.safely.doctor.AllHospitalsActivity
+import com.finalproj.safely.doctor.DoctorHomeActivity
+import com.finalproj.safely.hospital.HospitalHomeActivity
+import com.finalproj.safely.patient.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -12,11 +19,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+        val sharedPrefFile = "kotlin_shared_preference"
+        var sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
+            Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("Token", "")!!
+        val user_type = sharedPreferences.getString("user_type", "")!!
+
         GlobalScope.launch {
             delay(4000L)
-            val intent = Intent(this@MainActivity, BookAppointmentActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (token.isEmpty()) {
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else if(user_type == "patient") {
+                val intent = Intent(this@MainActivity, PatientHomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else if(user_type == "doctor") {
+                val intent = Intent(this@MainActivity, DoctorHomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else if(user_type == "hospital") {
+                val intent = Intent(this@MainActivity, HospitalHomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
         }
 } }
 
