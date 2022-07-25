a71dfc8cc30c57d3caaabe7d2ff28bfc1d2dc2b3
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-25 23:14:55 +0300

    Backend: Implemented addAppointment function

--- Backend/src/patient/service.js
+++ Backend/src/patient/service.js
@@ -1,4 +1,5 @@
 const Patient = require("../../model/Patient");
+const Appointment = require("../../model/Appointment");
 
 async function addPatient(body) {
     const {
@@ -21,8 +22,33 @@ async function addPatient(body) {
 
     return await patient.save();
 }
+async function addAppointment(body) {
+    const {
+        date,
+        time,
+        hospital_name,
+        hospital_id,
+        patient_name,
+        patient_id,
+        doctor_name,
+        doctor_id
+    } = body;
 
+    const appointment = new Appointment({
+        date,
+        time,
+        hospital_name,
+        hospital_id,
+        patient_name,
+        patient_id,
+        doctor_name,
+        doctor_id
+    });
+
+    return await appointment.save();
+}
 
 module.exports = {
     addPatient,
+    addAppointment
 };
