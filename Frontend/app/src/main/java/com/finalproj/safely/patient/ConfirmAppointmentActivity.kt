d60e0a00106ddd9bf7d09beadd244b14222c6e34
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-26 18:37:58 +0300

    Frontend: Passed appointment info to homepage

--- Frontend/app/src/main/java/com/finalproj/safely/patient/ConfirmAppointmentActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/ConfirmAppointmentActivity.kt
@@ -1,6 +1,7 @@
 package com.finalproj.safely.patient
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,32 +16,33 @@ import com.finalproj.safely.user.RestApiService
 import com.finalproj.safely.user.UserInfo
 
 class ConfirmAppointmentActivity : AppCompatActivity() {
+    lateinit var appointment_day: String
+    lateinit var appointment_time: String
+    lateinit var doctor_id: String
+    lateinit var doctor_name: String
+    lateinit var hospital_id: String
+    lateinit var hospital_name: String
+    lateinit var patient_id: String
+    lateinit var patient_name: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirm_appointment)
 
 
-        val appointment_day = intent.getStringExtra("appointment_day")
-        val appointment_time = intent.getStringExtra("appointment_time")
-        val doctor_id = intent.getStringExtra("doctor_id")
-        val doctor_name = intent.getStringExtra("doctor_name")
-        val hospital_id = intent.getStringExtra("hospital_id")
-        val hospital_name = intent.getStringExtra("hospital_name")
+        appointment_day = intent.getStringExtra("appointment_day")!!
+        appointment_time = intent.getStringExtra("appointment_time")!!
+        doctor_id = intent.getStringExtra("doctor_id")!!
+        doctor_name = intent.getStringExtra("doctor_name")!!
+        hospital_id = intent.getStringExtra("hospital_id")!!
+        hospital_name = intent.getStringExtra("hospital_name")!!
 
         val sharedPrefFile = "kotlin_shared_preference"
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
-        val patient_id = sharedPreferences.getString("patient_id", "")!!
-        val patient_name = sharedPreferences.getString("Name", "")!!
-
-        Log.d("DAY", appointment_day!!)
-        Log.d("TIME", appointment_time!!)
-        Log.d("DOCTOR_ID", doctor_id!!)
-        Log.d("DOCTOR_NAME", doctor_name!!)
-        Log.d("PATIENT_ID", patient_id)
-        Log.d("PATIENT_NAME", patient_name)
-        Log.d("HOSPITAL_ID", hospital_id!!)
-        Log.d("HOSPITAL_NAME", hospital_name!!)
+        patient_id = sharedPreferences.getString("patient_id", "")!!
+        patient_name = sharedPreferences.getString("Name", "")!!
 
         val day = findViewById<TextView>(R.id.appointment_date)
         val time = findViewById<TextView>(R.id.appointment_time)
@@ -77,8 +79,6 @@ class ConfirmAppointmentActivity : AppCompatActivity() {
                 patient_id,
                 patient_name)
         }
-
-
     }
 
     private fun confirm_appointment(
@@ -113,5 +113,12 @@ class ConfirmAppointmentActivity : AppCompatActivity() {
 
             }
         }
+        val intent =
+            Intent(this@ConfirmAppointmentActivity, PatientHomeActivity::class.java)
+        intent.putExtra("appointment_day", appointment_day)
+        intent.putExtra("appointment_time", appointment_time)
+        intent.putExtra("doctor_name", doctor_name)
+        intent.putExtra("hospital_name", hospital_name)
+        startActivity(intent)
     }
 }
--- Frontend/app/src/main/java/com/finalproj/safely/patient/PatientHomeActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/patient/PatientHomeActivity.kt
@@ -3,14 +3,18 @@ package com.finalproj.safely.patient
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import com.finalproj.safely.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PatientHomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_home)
@@ -22,10 +26,48 @@ class PatientHomeActivity : AppCompatActivity() {
         val token = sharedPreferences.getString("Token", "")
         Log.d("YEYYY", "" + token)
         val name = sharedPreferences.getString("Name", "")
-//        val name = intent.getStringExtra("User name").toString()
-        val textView: TextView = findViewById(R.id.user_name) as TextView
+        val textView = findViewById<TextView>(R.id.user_name)
         textView.text = name + "!"
-        Log.d("HUDA", name + "!")
+        if (name != null) {
+            Log.d("HUDA", name)
+        }
+        var appointment_day = ""
+        var appointment_time = ""
+        var doctor_name = ""
+        var hospital_name = ""
+
+
+        intent.getStringExtra("appointment_day")?.let {
+            appointment_day = it
+        }
+        intent.getStringExtra("appointment_time")?.let {
+            appointment_time = it
+        }
+        intent.getStringExtra("doctor_name")?.let {
+            doctor_name = it
+        }
+        intent.getStringExtra("hospital_name")?.let {
+            hospital_name = it
+        }
+        val home_date = findViewById<TextView>(R.id.home_date)
+        val hospital = findViewById<TextView>(R.id.hospital_name)
+        val upcoming_appointment = findViewById<TextView>(R.id.upcoming)
+
+        if (appointment_day != "" && appointment_time != "" && doctor_name != "" && hospital_name != "") {
+            val date= "$appointment_day, $appointment_time"
+            home_date.text = date
+            val location= "$hospital_name Hospital / Dr. $doctor_name"
+            hospital.setText(location)
+        }
+        else{
+            upcoming_appointment.text = "No Scheduled Appointments"
+            upcoming_appointment.setPadding(0, 20, 0, 0);
+            hospital.text=""
+            home_date.text="Discover the nearby hospitals and doctors and book an appointment"
+            val imageView = findViewById<ImageView>(R.id.loc)
+            imageView.visibility = ImageView.GONE
+        }
+
 
 
     }
