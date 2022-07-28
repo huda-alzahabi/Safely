9496c2b0a49e3bbe7a8256630caecdf6aebced26
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-28 11:40:27 +0300

    Frontend: Styled DoctorHomeActivity

--- Frontend/app/src/main/java/com/finalproj/safely/doctor/DoctorHomeActivity.kt
+++ Frontend/app/src/main/java/com/finalproj/safely/doctor/DoctorHomeActivity.kt
@@ -11,27 +11,51 @@ import android.view.Window
 import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.finalproj.safely.R
+import com.finalproj.safely.patient.HospitalResponse
+import com.finalproj.safely.patient.NearbyHospitalsAdapter
+import com.finalproj.safely.patient.TopSpacingItemDecoration
 import com.finalproj.safely.user.MainActivity
 import com.finalproj.safely.user.RestApiService
 
 class DoctorHomeActivity : AppCompatActivity() {
+    private lateinit var appointmentsAdapter: AppointmentsAdapter
+    private lateinit var appointmentsList: List<AppointmentResponse>
 
     val sharedPrefFile = "kotlin_shared_preference"
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Doctor);
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_home)
+
         var sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
         val token = sharedPreferences.getString("Token", "")!!
         val doctorId=sharedPreferences.getString("doctor_id", "")!!
         val apiService = RestApiService()
         apiService.getAppointmentsByDrId(doctorId,token) {
+            appointmentsList=it as List<AppointmentResponse>
+            appointmentsAdapter.submitList(appointmentsList)
             Log.d("appointments", it.toString())
         }
+        initRecyclerView()
 
+    }
+
+    private fun initRecyclerView() {
+
+        val recyclerView = findViewById<RecyclerView>(R.id.doctor_recycler_view)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@DoctorHomeActivity)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecorator)
+            appointmentsAdapter = AppointmentsAdapter()
+            adapter = appointmentsAdapter
+        }
     }
     //override oncreate options menu
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
