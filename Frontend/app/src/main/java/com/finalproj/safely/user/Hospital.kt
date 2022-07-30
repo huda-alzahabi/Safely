65cc570b1fecc1089029901c5a5ae69cd524a1a7
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-07-31 00:25:22 +0300

    Frontend: Created Hospital data class

---
+++ Frontend/app/src/main/java/com/finalproj/safely/user/Hospital.kt
@@ -0,0 +1,31 @@
+package com.finalproj.safely.user
+
+import com.finalproj.safely.patient.Location
+import com.finalproj.safely.user.UserInfo
+
+data class Hospital(
+    val _id: String,
+    val phone_number: String,
+    val address: Address,
+    val hospital_location: Location,
+    val distance: String,
+    val user: String,
+    val doctors: ArrayList<String>,
+    val outpatient_clinic: String,
+) {
+    override fun toString(): String {
+        return "Hospital(_id='$_id',phone_number='$phone_number',  address='$address',hospital_location='$hospital_location', user='$user', doctors='$doctors',distance='$distance')"
+    }
+
+    data class Address(
+        var country: String,
+        var city: String,
+        var street: String,
+        var building: String,
+    ) {
+        override fun toString(): String {
+            return "Address(country='$country', city='$city', street='$street', building='$building')"
+        }
+    }
+}
+
