61ba877af001687b9b2ec4e6c9f85fe0ddd9fbaa
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-02 00:11:28 +0300

    Frontend-Admin: Created side nav

---
+++ react-admin/src/components/SideNav.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "rsuite/dist/rsuite.min.css";
+import { Nav, Sidenav } from "rsuite/";
+import Home from "@rsuite/icons/legacy/Home";
+import HospitalO from "@rsuite/icons/legacy/HospitalO";
+import logo from "../assets/logo.png";
+import UserMd from "@rsuite/icons/legacy/UserMd";
+import Bed from "@rsuite/icons/legacy/Bed";
+import Percent from "@rsuite/icons/legacy/Percent";
+
+const SideNav = () => { 
+const [expand, setExpand] = useState(true);
+const [activeKey, setActiveKey] = useState("1");
+
+return (
+	<div>
+		<img className="logo" src={logo} alt="" />
+
+		<div className="sidenav" >
+		<Sidenav className="item" expanded={expand} appearance="inverse ">
+			<Sidenav.Body className="item">
+			<Nav activeKey={activeKey} onSelect={setActiveKey}>
+				<Nav.Item className="item" eventKey="1" icon={<Home />}>
+				All Users
+				</Nav.Item>
+				<Nav.Item className="item" eventKey="2" icon={<Percent />}>
+				Statistics
+				</Nav.Item>
+                <Nav.Item className="item" eventKey="3" icon={<HospitalO />}>
+				Hospitals
+				</Nav.Item>
+                <Nav.Item className="item" eventKey="4" icon={<Bed />}>
+				Patients
+				</Nav.Item>
+                <Nav.Item className="item" eventKey="5" icon={<UserMd />}>
+				Doctors
+				</Nav.Item>
+				</Nav>
+			</Sidenav.Body >
+			<Sidenav.Toggle className="item"
+			expanded={expand}
+			onToggle={(expanded) => setExpand(expanded)}
+			/>
+		</Sidenav>
+		</div>
+	</div>
+);
+}
+export default SideNav;
