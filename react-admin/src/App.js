c2c82e9c4abadc72dc1f6cc3822727df086c9604
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-01 16:21:29 +0300

    Admin-Frontend: Added routes in App.js

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,13 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login";
+import PieChart from "./components/PieChart";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
+    <BrowserRouter>
+        <div className="App">
+
+    <Routes>
+     <Route path="/login" element={<Login/>}></Route>
+     <Route path="/statistics" element={<PieChart/>}></Route>
       
+      </Routes>
       </div>
+      </BrowserRouter>
   );
 }
 
