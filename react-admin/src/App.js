0860478c77e35c6b543b2070245adf03f03505fc
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-02 00:12:39 +0300

    Frontend-Admin: Added route to statistics page

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import Login from "./pages/Login";
-import PieChart from "./components/PieChart";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Statistics from "./pages/Statistics";
 
+
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="App">
-
-    <Routes>
-     <Route path="/login" element={<Login/>}></Route>
-     <Route path="/statistics" element={<PieChart/>}></Route>
-      
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/statistics" element={<Statistics />}></Route>
+        </Routes>
       </div>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
