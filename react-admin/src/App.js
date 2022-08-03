8fa25dac0338d377d1e307a25b24f3971672dc07
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-03 23:16:24 +0300

    Frontend-Admin: Added routes to the pages

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Statistics from "./pages/Statistics";
-
-
+import Patients from "./pages/Patients";
+import Hospitals from "./pages/Hospitals";
+import Doctors from "./pages/Doctors";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/statistics" element={<Statistics />}></Route>
+          <Route path="/patients" element={<Patients />}></Route>
+          <Route path="/hospitals" element={<Hospitals />}></Route>
+          <Route path="/doctors" element={<Doctors />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
