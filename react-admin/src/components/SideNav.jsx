c44b3e7a44f0a61eb01fcefecda782a2ff62087c
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-03 23:14:40 +0300

    Frontend-Admin: Added navigation to sidenav

--- react-admin/src/components/SideNav.jsx
+++ react-admin/src/components/SideNav.jsx
@@ -7,43 +7,74 @@ import logo from "../assets/logo.png";
 import UserMd from "@rsuite/icons/legacy/UserMd";
 import Bed from "@rsuite/icons/legacy/Bed";
 import Percent from "@rsuite/icons/legacy/Percent";
+import { Link, useNavigate } from "react-router-dom";
 
-const SideNav = () => { 
-const [expand, setExpand] = useState(true);
-const [activeKey, setActiveKey] = useState("1");
+const SideNav = () => {
+  const [expand, setExpand] = useState(false);
+  const [activeKey, setActiveKey] = useState("1");
+  let nav = useNavigate();
 
-return (
-	<div>
-		<img className="logo" src={logo} alt="" />
-
-		<div className="sidenav" >
-		<Sidenav className="item" expanded={expand} appearance="inverse ">
-			<Sidenav.Body className="item">
-			<Nav activeKey={activeKey} onSelect={setActiveKey}>
-				<Nav.Item className="item" eventKey="1" icon={<Home />}>
-				All Users
-				</Nav.Item>
-				<Nav.Item className="item" eventKey="2" icon={<Percent />}>
-				Statistics
-				</Nav.Item>
-                <Nav.Item className="item" eventKey="3" icon={<HospitalO />}>
-				Hospitals
-				</Nav.Item>
-                <Nav.Item className="item" eventKey="4" icon={<Bed />}>
-				Patients
-				</Nav.Item>
-                <Nav.Item className="item" eventKey="5" icon={<UserMd />}>
-				Doctors
-				</Nav.Item>
-				</Nav>
-			</Sidenav.Body >
-			<Sidenav.Toggle className="item"
-			expanded={expand}
-			onToggle={(expanded) => setExpand(expanded)}
-			/>
-		</Sidenav>
-		</div>
-	</div>
-);
-}
+  function goToPatients() {
+    nav("/patients");
+  }
+  function goToHospitals() {
+    nav("/hospitals");
+  }
+  function goToDoctors() {
+    nav("/doctors");
+  }
+  function goToStats() {
+    nav("/statistics");
+  }
+  return (
+    <div>
+      <img className="logo" src={logo} alt="" />
+      <div className="sidenav">
+        <Sidenav className="item" expanded={expand} appearance="inverse ">
+          <Sidenav.Body className="item">
+            <Nav activeKey={activeKey} onSelect={setActiveKey}>
+              <Nav.Item
+                className="item"
+                eventKey="1"
+                icon={<Percent />}
+                onClick={goToStats}
+              >
+                Statistics
+              </Nav.Item>
+              <Nav.Item
+                className="item"
+                eventKey="2"
+                icon={<HospitalO />}
+                onClick={goToHospitals}
+              >
+                Hospitals
+              </Nav.Item>
+              <Nav.Item
+                className="item"
+                eventKey="3"
+                icon={<Bed />}
+                onClick={goToPatients}
+              >
+                Patients
+              </Nav.Item>
+              <Nav.Item
+                className="item"
+                eventKey="4"
+                icon={<UserMd />}
+                onClick={goToDoctors}
+              >
+                Doctors
+              </Nav.Item>
+            </Nav>
+          </Sidenav.Body>
+          <Sidenav.Toggle
+            className="item"
+            expanded={expand}
+            onToggle={(expanded) => setExpand(expanded)}
+          />
+        </Sidenav>
+      </div>
+    </div>
+  );
+};
 export default SideNav;
