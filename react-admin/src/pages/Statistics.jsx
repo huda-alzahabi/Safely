6502e9c5e36b799ca009638437e62e513cefa1ca
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-04 07:54:43 +0300

    Frontend-Admin: Added logout to Statistics page

--- react-admin/src/pages/Statistics.jsx
+++ react-admin/src/pages/Statistics.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import UsersPieChart from "../components/UsersPieChart";
 import SpecialtiesPieChart from "../components/SpecialtiesPieChart";
 import SideNav from "../components/SideNav";
+import { TbLogout } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
+
 const Statistics = () => {
+  const nav = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("user_id");
+    localStorage.removeItem("Bearer");
+    nav("/");
+  };
+
   return (
+    <>
+    <TbLogout className="logout" role="button" onClick={logout} />
     <div className="charts">
       <SideNav />
       <UsersPieChart />
       <SpecialtiesPieChart />
     </div>
+    </>
   );
 };
 
