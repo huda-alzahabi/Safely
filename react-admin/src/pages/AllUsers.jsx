54028de281abd899ffdeec0fbb20cfa89c295141
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-04 07:49:56 +0300

    Frontend-Admin: Added logout button to AllUsers page

--- react-admin/src/pages/AllUsers.jsx
+++ react-admin/src/pages/AllUsers.jsx
@@ -3,9 +3,12 @@ import { useState, useEffect } from "react";
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import SideNav from "../components/SideNav";
+import { TbLogout } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
   var isUpdated = false;
+  const nav = useNavigate();
   const [users, setUsers] = useState([]);
   const getUsers = async () => {
     const res = await fetch("http://127.0.0.1:3030/api/user/auth/get", {
@@ -42,34 +45,42 @@ const Users = () => {
     alert("User Successfully Deleted");
     isUpdated = !isUpdated;
   };
+  const logout = () => {
+    localStorage.removeItem("user_id");
+    localStorage.removeItem("Bearer");
+    nav("/");
+  };
 
   return (
-    <div className="charts">
-      <SideNav />
-      <table className="titleMargin">
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>User Type</th>
-        </tr>
-        {users.map((user, i) => (
-          <tr key={i}>
-            <td>{user.name} </td>
-            <td>{user.email}</td>
-            <td>{user.userType} </td>
-            <td>
-              <FaEdit />
-            </td>
-            <td>
-              <MdDeleteForever
-                role="button"
-                onClick={() => deleteUser(user._id)}
-              />
-            </td>
+    <>
+      <TbLogout className="logout" role="button" onClick={logout} />
+      <div className="charts">
+        <SideNav />
+        <table className="titleMargin">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Type</th>
           </tr>
-        ))}
-      </table>
-    </div>
+          {users.map((user, i) => (
+            <tr key={i}>
+              <td>{user.name} </td>
+              <td>{user.email}</td>
+              <td>{user.userType} </td>
+              <td>
+                <FaEdit />
+              </td>
+              <td>
+                <MdDeleteForever
+                  role="button"
+                  onClick={() => deleteUser(user._id)}
+                />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </>
   );
 };
 export default Users;
