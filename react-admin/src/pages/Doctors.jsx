00971c83255df729cca3d6e708199fa4200d043e
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-08-04 07:54:50 +0300

    Frontend-Admin: Added logout to Doctors page

--- react-admin/src/pages/Doctors.jsx
+++ react-admin/src/pages/Doctors.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { FaEdit } from "react-icons/fa";
-import { MdDeleteForever } from "react-icons/md";
 import SideNav from "../components/SideNav";
-import {TbLogout} from "react-icons/tb"
+import { TbLogout } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 
 const Doctors = () => {
+  const nav = useNavigate();
   const [doctors, setDoctors] = useState([]);
   const getDoctors = async () => {
     const res = await fetch("http://127.0.0.1:3030/api/doctor/all", {
@@ -27,37 +27,35 @@ const Doctors = () => {
   }, []);
   console.log(doctors);
 
+  const logout = () => {
+    localStorage.removeItem("user_id");
+    localStorage.removeItem("Bearer");
+    nav("/");
+  };
+
   return (
     <>
-    <TbLogout className="logout"/>
-    <div className="charts">
-      <SideNav />
-      <table className="titleMargin">
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Profession</th>
-          <th>Years of Experience</th>
-        </tr>
-        {doctors.map(
-          (doctor, i) =>
-            doctor.user !== null && (
-              <tr key={i}>
-                <td>{doctor.user.name} </td>
-                <td>{doctor.user.email} </td>
-                <td>{doctor.specialty}</td>
-                <td>{doctor.years_of_experience} years</td>
-                <td>
-                  <FaEdit />
-                </td>
-                <td>
-                  <MdDeleteForever />
-                </td>
-              </tr>
-            )
-        )}
-      </table>
-    </div>
+      <TbLogout className="logout" role="button" onClick={logout} />
+      <div className="charts">
+        <SideNav />
+        <table className="titleMargin">
+          <tr>
+            <th>Name</th>
+            <th>Profession</th>
+            <th>Years of Experience</th>
+          </tr>
+          {doctors.map(
+            (doctor, i) =>
+              doctor.user !== null && (
+                <tr key={i}>
+                  <td>{doctor.user.name} </td>
+                  <td>{doctor.specialty}</td>
+                  <td>{doctor.years_of_experience} years</td>
+                </tr>
+              )
+          )}
+        </table>
+      </div>
     </>
   );
 };
